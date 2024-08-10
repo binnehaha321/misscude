07e36ec4f5dd01e200a87130d0ec88085c77559d
binnehaha321 <workhanh79@gmail.com>
2024-08-10 09:19:54 +0700

    Fix bug flickering UI in post detail page

--- frontend/src/pages/Post.tsx
+++ frontend/src/pages/Post.tsx
@@ -5,12 +5,15 @@ import { useSinglePost } from '../hooks/usePost'
 
 import { SinglePost } from '../components/Checkin/SinglePost'
 import NotFound from '../components/NotFound'
+import { SinglePostSkeleton } from '../components/common/Skeleton'
 
 const Post = () => {
 	const { id } = useParams()
-	const { post } = useSinglePost(id as string)
+	const { post, isLoading } = useSinglePost(id as string)
 
+	if (isLoading) return <SinglePostSkeleton />
 	if (!id || !post) return <NotFound />
+
 	return (
 		<Box
 			py={3}
